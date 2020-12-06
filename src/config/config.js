35a41101d74678c6f0f280c18ac32b8c700a8427
Arun Kalya Pranesh Rao <akalya@dataguard.de>
2020-12-06 14:02:37 +0100

    Replaced config with ENV

--- src/config/config.js
+++ src/config/config.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: "postgres",
-    password: "123",
-    database: "books",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DS_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
   test: {
-    username: "postgres",
-    password: "123",
-    database: "books_test",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DS_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
   production: {
-    username: "postgres",
-    password: "PG@123",
-    database: "books",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DS_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
